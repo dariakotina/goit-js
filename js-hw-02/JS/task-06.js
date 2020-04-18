610ce20b1b2dfde0d5bec38710ee747cb77e0202
Your Name <you@example.com>
2020-04-18 18:40:17 +0300

    v 1.0

---
+++ js-hw-02/JS/task-06.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let input;
+const numbers = [];
+let total = 0;
+
+do {
+    input = prompt('Please enter number...');
+    if (isNaN(input)) {
+        alert(`Было введено не число, попробуйте еще раз`);
+        continue;
+    }
+    numbers.push(input);
+} while (input !== null);
+
+for (const number of numbers) {
+     total += Number(number);
+}
+alert(`Общая сумма чисел равна ${total}`);
+ 
---
+++ js-hw-02/JS/task-07.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+const isLoginValid = function(login) {
+  return (login.length < 4 || login.length > 16) ? false : true;
+};
+
+const isLoginUnique = function(allLogins, login) {
+  return allLogins.includes(login) ? false: true;
+};
+
+const addLogin = function(allLogins, login) {
+  if (!isLoginValid(login)) {
+    return "Ошибка! Логин должен быть от 4 до 16 символов";
+  }
+  if (!isLoginUnique(allLogins, login)) {
+    return "Такой логин уже используется!";
+  } else {
+      logins.push(login);
+      return 'Логин успешно добавлен';
+    }  
+  };
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
+console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
+console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
